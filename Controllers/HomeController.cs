73281ea4f72bd031e4962287d3040a0374d7f91c
xochitl0191 <xochitlfm@gmail.com>
2018-02-22 16:08:30 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using xochxoch.Models;
+
+namespace xochxoch.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.selectedItem = "about";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.selectedItem = "contact";
+
+            return View();
+        }
+
+        
+
+        public IActionResult Gallery()
+        {
+            ViewBag.selectedItem = "gallery";
+
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            ViewBag.selectedItem = "shop";
+
+            return View();
+        }
+
+
+    }
+}
